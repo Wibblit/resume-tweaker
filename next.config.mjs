917985cbd296dce78d64eb9ddc57d5ba621add0f
vanamuthuV <vanamuthuvana22@gmail.com>
2024-10-12 09:53:32 +0530

    Blogs Single Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: false, // keeping reactStrictMode off
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '30mb', // set to 30 MB
+    },
+  },
 };
 
 export default nextConfig;
