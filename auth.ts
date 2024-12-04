63b6a28588c78a3007a6747e037e42e934e01338
roopsagar-k <roopsagarudayar9854@gmail.com>
2024-12-04 08:50:40 +0530

    revalidate path update

--- auth.ts
+++ auth.ts
@@ -44,7 +44,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     async signIn({ user, account, profile }) {
       console.log("/home revalidated on signIn")
-      revalidatePath("/home", "layout");
+      revalidatePath("/", "layout");
       return true;
     }
   }
