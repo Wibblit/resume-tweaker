be6abb200d8b9063fc00d2af13be7833ce500723
roopsagar-k <roopsagarudayar9854@gmail.com>
2025-01-01 22:15:52 +0530

    drop down command update

--- components/Interview/adaptive-interview.tsx
+++ components/Interview/adaptive-interview.tsx
@@ -209,6 +209,7 @@ export default function AdaptiveInterview({
         }),
       });
       const data = await result.json();
+      setAudioBlob(null);
       if (!result.ok) {
         toast({
           title: `Error ${result.status}`,
@@ -532,6 +533,7 @@ export default function AdaptiveInterview({
               setIsRecording={setIsRecording}
               setAudioBlob={setAudioBlob}
               mediaRecorderRef={mediaRecorderRef}
+              audioBlob={audioBlob}
             />
             <AudioVisualization isRecording={isRecording} />
             <div className="flex justify-between mt-4">
