d13f4a96ac1c1e00efe7613bb4e59f1f56690cc4
roopsagar-k <roopsagarudayar9854@gmail.com>
2025-01-11 09:30:15 +0530

    onboarding redirection

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { auth } from "@/auth";
 import Onboarding from "@/components/onboard";
+import { redirect } from "next/navigation";
 
 const OnBoarding = async () => {
   const session = await auth();
+  if (!session?.isNewUser) redirect("/home");
   return <Onboarding />;
 };
 
