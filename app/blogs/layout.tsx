a636a9a1535117684b59d4adcab6e0b091acea40
sairamkumarm21 <sairamkumarm21@gmail.com>
2025-02-04 14:09:50 +0530

    phone blog double nav solved

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -1,9 +1,6 @@
-import { LandingNav } from "@/components/LandingNav";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <LandingNav />
       <main className="flex-grow md:pt-0 ">{children}</main>
     </div>
   );
