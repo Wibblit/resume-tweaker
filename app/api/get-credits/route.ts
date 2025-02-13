0d4e3d445565f4e09ea15f89bdde4e05a198b30a
roopsagar-k <roopsagarudayar9854@gmail.com>
2025-02-13 20:20:01 +0530

    dynamic issue resolved

--- app/api/get-credits/route.ts
+++ app/api/get-credits/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { auth } from "@/auth";
 import { NextRequest, NextResponse } from "next/server";
 import { rateLimiter } from "@/lib/rateLimiter";
--- app/api/verify-cover-slots/route.ts
+++ app/api/verify-cover-slots/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { asyncHandler } from "@/lib/apiRouteHelpers/asyncHandler";
 import { NextRequest, NextResponse } from "next/server";
 import { ApiError } from "@/lib/apiRouteHelpers/errorHandler";
